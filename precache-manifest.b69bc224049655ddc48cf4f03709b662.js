6ce571dde7f2c02837f5ebe6d3be377bc5f50269
k- <mahat.mejor@gmail.com>
2020-03-30 20:22:00 -0600

    Updates

---
+++ precache-manifest.b69bc224049655ddc48cf4f03709b662.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4d539723bdcba676f6e0213d118a96c",
+    "url": "/spotifu/index.html"
+  },
+  {
+    "revision": "43a06572e00acddbebc4",
+    "url": "/spotifu/static/css/2.440ac8e8.chunk.css"
+  },
+  {
+    "revision": "1c6bdfff7ac2a7f0385c",
+    "url": "/spotifu/static/css/main.7bad8d5b.chunk.css"
+  },
+  {
+    "revision": "43a06572e00acddbebc4",
+    "url": "/spotifu/static/js/2.6f80ffed.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/spotifu/static/js/2.6f80ffed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c6bdfff7ac2a7f0385c",
+    "url": "/spotifu/static/js/main.f25eff27.chunk.js"
+  },
+  {
+    "revision": "12114a988589a4de2d7a",
+    "url": "/spotifu/static/js/runtime-main.be855e04.js"
+  },
+  {
+    "revision": "659ef56b081cef83c641fa34aa250b65",
+    "url": "/spotifu/static/media/pm.659ef56b.mp3"
+  }
+]);
